c9a85275c0e9576d04efcb5b0da74852e594354a
Apurv Patel <apurvpatel103@icloud.com>
2025-11-12 15:59:16 -0500

    Initial Execution

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,7 +10,8 @@ export const databaseConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'jaiman123',
   database: process.env.DB_NAME || 'bookngo_db',
-  entities: ['dist/**/*.entity{.ts,.js}'], // Auto-load entities
+  entities: [__dirname + '/../**/*.entity.{ts,js}'], // Match entities in both dev (ts) and prod (js)
+  autoLoadEntities: true, // Let Nest load entities from forFeature() automatically
   synchronize: process.env.DB_SYNC === 'true', // Use true only in development!
   logging: process.env.DB_LOG === 'true', // Enable logging if needed
 };
