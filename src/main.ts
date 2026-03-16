f90e7c2a62a53126ae3ce4ef119ebecc3aab797c
JaimanJoshi25 <jaiman.joshi@hnresearch.ai>
2026-03-16 12:34:26 -0400

    Cookie Parser fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ loadEnv();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './modules/cms/auth/auth.guards';
 import { Reflector } from '@nestjs/core';
